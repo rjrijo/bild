1746e5112a8c2b8d2800e97c71c8a356c87bd61c
Rijo Raju <rijo_raju@outlook.com>
2015-08-31 12:43:33 +0530

    no message

---
+++ App/app/src/main/java/com/rijofaith/app/app/AppConfig.java
@@ -0,0 +1,12 @@
+package com.rijofaith.app.app;
+
+/**
+ * Created by rijo on 31-Aug-15.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://thecapitalcitychurch.16mb.com/";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://thecapitalcitychurch.16mb.com/";
+}
