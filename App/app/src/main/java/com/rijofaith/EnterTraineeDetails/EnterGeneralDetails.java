71e563a470fc0e11b04da3bab8936cb3cb683c49
Rijo <rj.rijo@gmail.com>
2015-10-09 00:54:20 +0530

    added code to work with spinner
    i dont know what i have did im still getting error..

--- App/app/src/main/java/com/rijofaith/EnterTraineeDetails/EnterGeneralDetails.java
+++ App/app/src/main/java/com/rijofaith/EnterTraineeDetails/EnterGeneralDetails.java
@@ -1,17 +1,107 @@
 package com.rijofaith.EnterTraineeDetails;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.rijofaith.app.R;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
  * Created by rijo on 16-Sep-15.
  */
 public class EnterGeneralDetails extends Fragment {
+
+    ArrayList<String> listItems=new ArrayList<>();
+    ArrayAdapter<String> adapter;
+    Spinner sp;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_general_details);
+        sp=(Spinner)findViewById(R.id.spinner);
+        adapter=new ArrayAdapter<String>(this,R.layout.spinner_layout,R.id.textView8,listItems);
+        sp.setAdapter(adapter);
+
+    }
+    public void onStart(){
+        super.onStart();
+        BackTask bt=new BackTask();
+        bt.execute();
+    }
+
+    private class BackTask extends AsyncTask<Void,Void,Void> {
+        ArrayList<String> list;
+        protected void onPreExecute(){
+            super.onPreExecute();
+            list=new ArrayList<>();
+        }
+
+        protected Void doInBackground(Void...params){
+            InputStream is=null;
+            String result="";
+            try{
+                HttpClient httpclient=new DefaultHttpClient();
+                HttpPost httppost= new HttpPost("http://thecapitalcitychurch.16mb.com/new/CohortName.php");
+                HttpResponse response=httpclient.execute(httppost);
+                HttpEntity entity = response.getEntity();
+                // Get our response as a String.
+                is = entity.getContent();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+
+            //convert response to string
+            try{
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is,"utf-8"));
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    result+=line;
+                }
+                is.close();
+                //result=sb.toString();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            // parse json data
+            try{
+                JSONArray jArray =new JSONArray(result);
+                for(int i=0;i<jArray.length();i++){
+                    JSONObject jsonObject=jArray.getJSONObject(i);
+                    // add interviewee name to arraylist
+                    list.add(jsonObject.getString("iname"));
+                }
+            }
+            catch(JSONException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+        protected void onPostExecute(Void result){
+            listItems.addAll(list);
+            adapter.notifyDataSetChanged();
+        }
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //Inflate the layout for this fragment
