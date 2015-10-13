0f40622b1859b0eb1a2a23304565491826c49e9b
rijo <rj.rijo@gmail.com>
2015-10-13 12:04:56 +0530

    spinner working

--- App/app/src/main/java/com/rijofaith/app/EnterGeneralDetails.java
+++ App/app/src/main/java/com/rijofaith/app/EnterGeneralDetails.java
@@ -1,11 +1,34 @@
 package com.rijofaith.app;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 
-public class EnterGeneralDetails extends Activity {
+public class EnterGeneralDetails extends ActionBarActivity {
+
+    ArrayList<String> listItems=new ArrayList<>();
+    ArrayAdapter<String> adapter;
+    Spinner sp;
 
 
     @Override
@@ -13,7 +36,68 @@ public class EnterGeneralDetails extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_details);
 
+        sp=(Spinner)findViewById(R.id.spinner22);
+        adapter=new ArrayAdapter<String>(this,R.layout.spinner_layout,R.id.txt,listItems);
+        sp.setAdapter(adapter);
+
 
     }
 
+    public void onStart() {
+        super.onStart();
+        BackTask bt = new BackTask();
+        bt.execute();
+    }
+
+    private class BackTask extends AsyncTask<Void,Void,Void> {
+        ArrayList<String> list;
+        protected void onPreExecute(){
+            super.onPreExecute();
+            list=new ArrayList<>();
+        }
+        protected Void doInBackground(Void...params){
+            InputStream is=null;
+            String result="";
+            try{
+                HttpClient httpclient=new DefaultHttpClient();
+                HttpPost httppost= new HttpPost("http://thecapitalcitychurch.16mb.com/new/CohortName.php");
+                HttpResponse response=httpclient.execute(httppost);
+                HttpEntity entity = response.getEntity();
+                // Get our response as a String.
+                is = entity.getContent();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+
+            //convert response to string
+            try{
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is,"utf-8"));
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    result+=line;
+                }
+                is.close();
+                //result=sb.toString();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            // parse json data
+            try{
+                JSONArray jArray =new JSONArray(result);
+                for(int i=0;i<jArray.length();i++){
+                    JSONObject jsonObject=jArray.getJSONObject(i);
+                    // add interviewee name to arraylist
+                    list.add(jsonObject.getString("Cohort_Name"));
+                }
+            }
+            catch(JSONException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+        protected void onPostExecute(Void result){
+            listItems.addAll(list);
+            adapter.notifyDataSetChanged();
+        }
+    }
 }
