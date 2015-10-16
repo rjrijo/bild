f9b0dfd033d962cc8366997d39384ad743ee3746
rijo <rj.rijo@gmail.com>
2015-10-16 10:37:48 +0530

    with static spinner data

--- App/app/src/main/java/com/rijofaith/app/EnterGeneralDetails.java
+++ App/app/src/main/java/com/rijofaith/app/EnterGeneralDetails.java
@@ -1,103 +1,48 @@
 package com.rijofaith.app;
 
-import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 
-public class EnterGeneralDetails extends Activity {
+public class EnterGeneralDetails extends ActionBarActivity {
 
-    ArrayList<String> listItems=new ArrayList<>();
-    ArrayAdapter<String> adapter;
-    Spinner sp;
+    Spinner spn_title;
+    Spinner spn_cohort;
+    Spinner spn_cohort;
+    List<String> List_title=new ArrayList<String>();
+    List<String> List_cohort=new ArrayList<String>();
 
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_details);
-
-        sp=(Spinner)findViewById(R.id.spinner22);
-        adapter=new ArrayAdapter<String>(this,R.layout.spinner_layout,R.id.txt,listItems);
-        sp.setAdapter(adapter);
-
-
+        spn_title=(Spinner)findViewById(R.id.spinner23);
+        spn_cohort=(Spinner)findViewById(R.id.spinner22);
+        populatespinner();
     }
 
-    public void onStart() {
-        super.onStart();
-        BackTask bt = new BackTask();
-        bt.execute();
+    public void populatespinner() {
+        List_title.add("Mr.");
+        List_title.add("Mrs.");
+
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,List_title);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spn_title.setAdapter(adapter);
+        ArrayAdapter<CharSequence> adapter2= ArrayAdapter.createFromResource(this,R.array.Cohort_name,android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spn_cohort.setAdapter(adapter2);
     }
 
-    private class BackTask extends AsyncTask<Void,Void,Void> {
-        ArrayList<String> list;
-        protected void onPreExecute(){
-            super.onPreExecute();
-            list=new ArrayList<>();
-        }
-        protected Void doInBackground(Void...params){
-            InputStream is=null;
-            String result="";
-            try{
-                HttpClient httpclient=new DefaultHttpClient();
-                HttpPost httppost= new HttpPost("http://thecapitalcitychurch.16mb.com/new/CohortName.php");
-                HttpResponse response=httpclient.execute(httppost);
-                HttpEntity entity = response.getEntity();
-                // Get our response as a String.
-                is = entity.getContent();
-            }catch(IOException e){
-                e.printStackTrace();
-            }
 
-            //convert response to string
-            try{
-                BufferedReader reader = new BufferedReader(new InputStreamReader(is,"utf-8"));
-                String line = null;
-                while ((line = reader.readLine()) != null) {
-                    result+=line;
-                }
-                is.close();
-                //result=sb.toString();
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-            // parse json data
-            try{
-                JSONArray jArray =new JSONArray(result);
-                for(int i=0;i<jArray.length();i++){
-                    JSONObject jsonObject=jArray.getJSONObject(i);
-                    // add interviewee name to arraylist
-                    list.add(jsonObject.getString("Cohort_Name"));
-                }
-            }
-            catch(JSONException e){
-                e.printStackTrace();
-            }
-            return null;
-        }
-        protected void onPostExecute(Void result){
-            listItems.addAll(list);
-            adapter.notifyDataSetChanged();
-        }
-    }
 }
+
+
