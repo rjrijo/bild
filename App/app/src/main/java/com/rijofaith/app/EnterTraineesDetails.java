25e37adc4fb1a1cf78ef81901969407978621cb5
rijo <rj.rijo@gmail.com>
2015-10-23 16:20:24 +0530

    final

--- App/app/src/main/java/com/rijofaith/app/EnterTraineesDetails.java
+++ App/app/src/main/java/com/rijofaith/app/EnterTraineesDetails.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -35,7 +37,11 @@ public class EnterTraineesDetails extends Activity {
     Spinner spn_church;
     Spinner spn_designation;
 
-    CheckBox c1, c2, c3, c4;
+    CheckBox cb1, cb2, cb3, cb4;
+
+    RadioGroup rg1, rg2, rg3, rg4, rg5, rg6, rg7, rg8, rg9, rg10, rg11, rg12, rg13, rg14, rg15, rg16, rg17, rg18, rg19, rg20, rg21, rg22, rg23, rg24, rg25, rg26, rg27, rg28, rg29, rg30, rg31, rg32, rg33;
+
+    RadioButton rb1, rb2, rb3, rb4, rb5, rb6, rb7, rb8, rb9, rb10, rb11, rb12, rb13, rb14, rb15, rb16, rb17, rb18, rb19, rb20, rb21, rb22, rb23, rb24, rb25, rb26, rb27, rb28, rb29, rb30, rb31, rb32, rb33;
 
     List<String> List_title = new ArrayList<String>();
     List<String> List_cohort = new ArrayList<String>();
@@ -60,6 +66,33 @@ public class EnterTraineesDetails extends Activity {
     EditText personal_identification;
     EditText eportfolio_username;
     EditText eportfolio_password;
+    EditText date_of_enrollment;
+    EditText date_of_assessment;
+    EditText date_of_certification;
+    EditText center_of_training;
+    EditText no_of_book_finished;
+    EditText no_of_students;
+    EditText what_did_they_learn;
+    EditText what_did_learn_from_teaching_fp;
+    EditText date1;
+    EditText date2;
+    EditText date3;
+    EditText attachments;
+    EditText educational_qualification;
+    EditText biblical_qualification;
+    EditText last_bible_school_attended;
+    EditText date_of_repentance;
+    EditText date_of_water_baptism;
+    EditText religious_social_background;
+    EditText previous_ministry_experience;
+    EditText previous_christian_organization;
+    EditText other_support;
+    EditText ministry_interest;
+    EditText name_address_of_pastor_leader;
+    EditText testimony;
+    EditText date_of_marriage;
+    EditText spouse_name;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,10 +107,10 @@ public class EnterTraineesDetails extends Activity {
 
         populatespinner();
 
-        c1 =  (CheckBox) findViewById(R.id.checkBoxB_minus_bius);
-        c2 =  (CheckBox) findViewById(R.id.checkBoxB_minus_antioch_school);
-        c3 =  (CheckBox) findViewById(R.id.checkBoxM_minus_antioch_school);
-        c4 =  (CheckBox) findViewById(R.id.checkBoxD_minus_antioch_school);
+        cb1 =  (CheckBox) findViewById(R.id.checkBoxB_minus_bius);
+        cb2 =  (CheckBox) findViewById(R.id.checkBoxB_minus_antioch_school);
+        cb3 =  (CheckBox) findViewById(R.id.checkBoxM_minus_antioch_school);
+        cb4 =  (CheckBox) findViewById(R.id.checkBoxD_minus_antioch_school);
 
 
         cohort_leader = (EditText) findViewById(R.id.editText47);
@@ -98,6 +131,32 @@ public class EnterTraineesDetails extends Activity {
         personal_identification = (EditText) findViewById(R.id.editText62);
         eportfolio_username = (EditText) findViewById(R.id.editText20);
         eportfolio_password = (EditText) findViewById(R.id.editText21);
+        date_of_enrollment = (EditText) findViewById(R.id.editText20);
+        date_of_assessment = (EditText) findViewById(R.id.editText21);
+        date_of_certification = (EditText) findViewById(R.id.editText22);
+        center_of_training = (EditText) findViewById(R.id.editText23);
+        no_of_book_finished = (EditText) findViewById(R.id.editText24);
+        no_of_students = (EditText) findViewById(R.id.editText25);
+        what_did_they_learn = (EditText) findViewById(R.id.editText26);
+        what_did_learn_from_teaching_fp = (EditText) findViewById(R.id.editText27);
+        date1 = (EditText) findViewById(R.id.editText28);
+        date2 = (EditText) findViewById(R.id.editText32);
+        date3 = (EditText) findViewById(R.id.editText33);
+        attachments = (EditText) findViewById(R.id.editText34);
+        educational_qualification = (EditText) findViewById(R.id.editText30);
+        biblical_qualification = (EditText) findViewById(R.id.editText35);
+        last_bible_school_attended = (EditText) findViewById(R.id.editText36);
+        date_of_repentance = (EditText) findViewById(R.id.editText31);
+        date_of_water_baptism = (EditText) findViewById(R.id.editText39);
+        religious_social_background = (EditText) findViewById(R.id.editText37);
+        previous_ministry_experience = (EditText) findViewById(R.id.editText38);
+        previous_christian_organization = (EditText) findViewById(R.id.editText40);
+        other_support = (EditText) findViewById(R.id.editText41);
+        ministry_interest = (EditText) findViewById(R.id.editText42);
+        name_address_of_pastor_leader = (EditText) findViewById(R.id.editText43);
+        testimony = (EditText) findViewById(R.id.editText44);
+        date_of_marriage = (EditText) findViewById(R.id.editText45);
+        spouse_name = (EditText) findViewById(R.id.editText46);
     }
 
     public void populatespinner() {
@@ -139,45 +198,74 @@ public class EnterTraineesDetails extends Activity {
         String n = pin.getText().toString();
         String o = permanent_address.getText().toString();
         String p = personal_identification.getText().toString();
-        String q = null;
-        String r = null;
-        String s = eportfolio_username.getText().toString();
-        String t = eportfolio_password.getText().toString();
+        String q = eportfolio_username.getText().toString();
+        String r = eportfolio_password.getText().toString();
+        String s = date_of_enrollment.getText().toString();
+        String t = date_of_assessment.getText().toString();
+        String u = date_of_certification.getText().toString();
+        String v = center_of_training.getText().toString();
+        String w = no_of_book_finished.getText().toString();
+        String x = no_of_students.getText().toString();
+        String y = what_did_they_learn.getText().toString();
+        String z = what_did_learn_from_teaching_fp.getText().toString();
+        String a1 = date1.getText().toString();
+        String b1 = date2.getText().toString();
+        String c1 = date3.getText().toString();
+        String d1 = attachments.getText().toString();
+        String e1 = educational_qualification.getText().toString();
+        String f1 = biblical_qualification.getText().toString();
+        String g1 = last_bible_school_attended.getText().toString();
+        String h1 = date_of_repentance.getText().toString();
+        String i1 = date_of_water_baptism.getText().toString();
+        String j1 = religious_social_background.getText().toString();
+        String k1 = previous_ministry_experience.getText().toString();
+        String l1 = previous_christian_organization.getText().toString();
+        String m1 = other_support.getText().toString();
+        String n1 = ministry_interest.getText().toString();
+        String o1 = name_address_of_pastor_leader.getText().toString();
+        String p1 = testimony.getText().toString();
+        String q1 = date_of_marriage.getText().toString();
+        String r1 = spouse_name.getText().toString();
+
+        String s1 = null;
+        String t1 = null;
+
+
 
         //checkbox
-        if((c1.isChecked()==false))
+        if((cb1.isChecked()==false))
         {
-            q = null;
+            s1 = null;
         }
         else
         {
-            q = "B Minus BIUS";
+            s1 = "B Minus BIUS";
         }
 
-        if((c2.isChecked()==false) && (c3.isChecked()==false) && (c4.isChecked()==false))
+        if((cb2.isChecked()==false) && (cb3.isChecked()==false) && (cb4.isChecked()==false))
         {
-            r = null;
+            t1 = null;
         }
         else
         {
-            if(c2.isChecked())
+            if(cb2.isChecked())
             {
-                r="B Minus Antioch School";
+                t1="B Minus Antioch School";
             }
-            if(c3.isChecked())
+            if(cb3.isChecked())
             {
-                r=r.concat(" M Minus Antioch School");
+                t1=t1.concat(" M Minus Antioch School");
             }
-            if(c4.isChecked())
+            if(cb4.isChecked())
             {
-                r=r.concat(" D Minus Antioch School");
+                t1=t1.concat(" D Minus Antioch School");
             }
         }
 
-        insertToDatabase(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r,s,t);
+        insertToDatabase(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, a1, b1, c1, d1, e1, f1, g1, h1, i1, j1, k1, l1, m1, n1, o1, p1, q1, r1, s1, t1);
     }
 
-    private void insertToDatabase(String a, String b, String c, String d, String e, String f, String g, String h, String i, String j, String k, String l, String m, String n, String o, String p, String q, String r, String s, String t) {
+    private void insertToDatabase(String a, String b, String c, String d, String e, String f, String g, String h, String i, String j, String k, String l, String m, String n, String o, String p, String q, String r, String s, String t, String u, String v, String w, String x, String y, String z, String a1, String b1, String c1, String d1, String e1, String f1, String g1, String h1, String i1, String j1, String k1, String l1, String m1, String n1, String o1, String p1, String q1, String r1, String s1, String t1) {
         class SendPostReqAsyncTask extends AsyncTask<String, Void, String> {
             @Override
             protected String doInBackground(String... params) {
@@ -201,6 +289,32 @@ public class EnterTraineesDetails extends Activity {
                 String param_r = params[17];
                 String param_s = params[18];
                 String param_t = params[19];
+                String param_u = params[20];
+                String param_v = params[21];
+                String param_w = params[22];
+                String param_x = params[23];
+                String param_y = params[24];
+                String param_z = params[25];
+                String param_a1 = params[26];
+                String param_b1 = params[27];
+                String param_c1 = params[28];
+                String param_d1 = params[29];
+                String param_e1 = params[30];
+                String param_f1 = params[31];
+                String param_g1 = params[32];
+                String param_h1 = params[33];
+                String param_i1 = params[34];
+                String param_j1 = params[35];
+                String param_k1 = params[36];
+                String param_l1 = params[37];
+                String param_m1 = params[38];
+                String param_n1 = params[39];
+                String param_o1 = params[40];
+                String param_p1 = params[41];
+                String param_q1 = params[42];
+                String param_r1 = params[43];
+                String param_s1 = params[44];
+                String param_t1 = params[45];
 
 
                 String a = cohort_leader.getText().toString();
@@ -219,10 +333,35 @@ public class EnterTraineesDetails extends Activity {
                 String n = pin.getText().toString();
                 String o = permanent_address.getText().toString();
                 String p = personal_identification.getText().toString();
-                String q = null;
-                String r = null;
-                String s = eportfolio_username.getText().toString();
-                String t = eportfolio_password.getText().toString();
+                String q = eportfolio_username.getText().toString();
+                String r = eportfolio_password.getText().toString();
+                String s = date_of_enrollment.getText().toString();
+                String t = date_of_assessment.getText().toString();
+                String u = date_of_certification.getText().toString();
+                String v = center_of_training.getText().toString();
+                String w = no_of_book_finished.getText().toString();
+                String x = no_of_students.getText().toString();
+                String y = what_did_they_learn.getText().toString();
+                String z = what_did_learn_from_teaching_fp.getText().toString();
+                String a1 = date1.getText().toString();
+                String b1 = date2.getText().toString();
+                String c1 = date3.getText().toString();
+                String d1 = attachments.getText().toString();
+                String e1 = educational_qualification.getText().toString();
+                String f1 = biblical_qualification.getText().toString();
+                String g1 = last_bible_school_attended.getText().toString();
+                String h1 = date_of_repentance.getText().toString();
+                String i1 = date_of_water_baptism.getText().toString();
+                String j1 = religious_social_background.getText().toString();
+                String k1 = previous_ministry_experience.getText().toString();
+                String l1 = previous_christian_organization.getText().toString();
+                String m1 = other_support.getText().toString();
+                String n1 = ministry_interest.getText().toString();
+                String o1 = name_address_of_pastor_leader.getText().toString();
+                String p1 = testimony.getText().toString();
+                String q1 = date_of_marriage.getText().toString();
+                String r1 = spouse_name.getText().toString();
+
 
                 List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
                 nameValuePairs.add(new BasicNameValuePair("a", a));
@@ -245,6 +384,31 @@ public class EnterTraineesDetails extends Activity {
                 nameValuePairs.add(new BasicNameValuePair("r", r));
                 nameValuePairs.add(new BasicNameValuePair("s", s));
                 nameValuePairs.add(new BasicNameValuePair("t", t));
+                nameValuePairs.add(new BasicNameValuePair("u", u));
+                nameValuePairs.add(new BasicNameValuePair("v", v));
+                nameValuePairs.add(new BasicNameValuePair("w", w));
+                nameValuePairs.add(new BasicNameValuePair("x", x));
+                nameValuePairs.add(new BasicNameValuePair("y", y));
+                nameValuePairs.add(new BasicNameValuePair("z", z));
+                nameValuePairs.add(new BasicNameValuePair("a1", a1));
+                nameValuePairs.add(new BasicNameValuePair("b1", b1));
+                nameValuePairs.add(new BasicNameValuePair("c1", c1));
+                nameValuePairs.add(new BasicNameValuePair("d1", d1));
+                nameValuePairs.add(new BasicNameValuePair("e1", e1));
+                nameValuePairs.add(new BasicNameValuePair("f1", f1));
+                nameValuePairs.add(new BasicNameValuePair("g1", g1));
+                nameValuePairs.add(new BasicNameValuePair("h1", h1));
+                nameValuePairs.add(new BasicNameValuePair("i1", i1));
+                nameValuePairs.add(new BasicNameValuePair("j1", j1));
+                nameValuePairs.add(new BasicNameValuePair("k1", k1));
+                nameValuePairs.add(new BasicNameValuePair("l1", l1));
+                nameValuePairs.add(new BasicNameValuePair("m1", m1));
+                nameValuePairs.add(new BasicNameValuePair("n1", n1));
+                nameValuePairs.add(new BasicNameValuePair("o1", o1));
+                nameValuePairs.add(new BasicNameValuePair("p1", p1));
+                nameValuePairs.add(new BasicNameValuePair("q1", q1));
+                nameValuePairs.add(new BasicNameValuePair("r1", r1));
+
 
                 try {
                     HttpClient httpClient = new DefaultHttpClient();
@@ -275,7 +439,7 @@ public class EnterTraineesDetails extends Activity {
             }
         }
         SendPostReqAsyncTask sendPostReqAsyncTask = new SendPostReqAsyncTask();
-        sendPostReqAsyncTask.execute(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t);
+        sendPostReqAsyncTask.execute(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,a1,b1,c1,d1,e1,f1,g1,h1,i1,j1,k1,l1,m1,n1,o1,p1,q1,r1);
     }
 
 
